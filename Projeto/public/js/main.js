ac7c5d418dbbe67b0b55472a0ac60b77b721b6b2
JoaoZanelato <jgbzanelato@gmail.com>
2025-07-11 11:14:04 -0300

    ajuste caminhos / main.js

--- Projeto/public/js/main.js
+++ Projeto/public/js/main.js
@@ -1,40 +1,41 @@
-document.addEventListener('DOMContentLoaded', function(){
-    //Pega os elementos do modal
-    var modal = document.getElementById('imageModal');
-    var modalImg = document.getElementById('modalImage');
-    var captionText = document.getElementById('modalCaption');
+document.addEventListener("DOMContentLoaded", function () {
+  //Pega os elementos do modal
+  var modal = document.getElementById("imageModal");
 
-    if(!modal || !modalImg || !captionText) {
-        console.error("Elementos do modal não encontrados!")
-        return
+  // SÓ EXECUTA O CÓDIGO SE O MODAL EXISTIR NA PÁGINA ATUAL
+  if (modal) {
+    var modalImg = document.getElementById("modalImage");
+    var captionText = document.getElementById("modalCaption");
+
+    if (!modalImg || !captionText) {
+      console.error("Partes internas do modal não foram encontradas!");
+      return;
     }
 
     //adiciona um evento de clique a cada cartão de imagem
-    document.querySelectorAll('.image-card').forEach(card => {
-        card.onclick = function() {
-            const img = this.querySelector('img'); // Pega a imagem dentro do cartão clicado
-            modal.classList.add('visible');
-            modalImg.src = img.src; // Usa o URL da imagem clicada
-            captionText.innerHTML = img.alt; // Usa o texto alternativo como legenda
-        }
-    })
-    // Pega o elemento <span> que fecha o modal
-    var span = document.getElementsByClassName('modal-close')[0];
+    document.querySelectorAll(".image-card").forEach((card) => {
+      card.onclick = function () {
+        const img = this.querySelector("img");
+        modal.classList.add("visible");
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+      };
+    });
 
-    // Função para fechar o modal
-    function closeModal(){
-        modal.classList.remove('visible');
+    var span = document.getElementsByClassName("modal-close")[0];
+
+    function closeModal() {
+      modal.classList.remove("visible");
     }
 
-    // Quando o usuário clica no <span> (x), fecha o modal
-    if(span) {
-        span.onclick = closeModal;
+    if (span) {
+      span.onclick = closeModal;
     }
 
-    // Fecha o modal se o usuário clicar fora da imagem
-    window.onclick = function(event) {
-        if(event.target == modal) {
-            closeModal();
-        }
-    }
-})
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        closeModal();
+      }
+    };
+  }
+});
